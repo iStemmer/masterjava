8789a64e1777bc2afefa456488127b6c8831ebcd
iStemmer <mikhalev-pavel@yandex.ru>
2019-07-27 23:00:40 +0300

    11 3 HW10 JMS attach fix

--- services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailServiceExecutor.java
+++ services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailServiceExecutor.java
@@ -1,6 +1,5 @@
 package ru.javaops.masterjava.service.mail;
 
-import com.google.common.collect.ImmutableList;
 import lombok.extern.slf4j.Slf4j;
 import one.util.streamex.StreamEx;
 import ru.javaops.masterjava.ExceptionType;
