1a8c61613417f570b73531cbae6058f10db0e860
iStemmer <mikhalev-pavel@yandex.ru>
2019-07-27 22:59:40 +0300

    11 2 HW10 JMS attach

--- services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailServiceExecutor.java
+++ services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailServiceExecutor.java
@@ -1,8 +1,11 @@
 package ru.javaops.masterjava.service.mail;
 
+import com.google.common.collect.ImmutableList;
 import lombok.extern.slf4j.Slf4j;
 import one.util.streamex.StreamEx;
 import ru.javaops.masterjava.ExceptionType;
+import ru.javaops.masterjava.service.mail.util.MailUtils;
+import ru.javaops.masterjava.service.mail.util.MailUtils.MailObject;
 import ru.javaops.masterjava.web.WebStateException;
 import ru.javaops.masterjava.web.WsClient;
 
@@ -18,6 +21,20 @@ public class MailServiceExecutor {
     private static final String INTERRUPTED_BY_TIMEOUT = "+++ Interrupted by timeout";
 
     private static final ExecutorService mailExecutor = Executors.newFixedThreadPool(8);
+
+    public static void sendAsync(MailObject mailObject) {
+        Set<Addressee> addressees = MailUtils.split(mailObject.getUsers());
+        addressees.forEach(addressee ->
+                mailExecutor.submit(() -> {
+                    try {
+                        MailSender.sendTo(addressee, mailObject.getSubject(), mailObject.getBody(),
+                                ImmutableList.of(MailUtils.getAttachment(mailObject.getAttachName(), mailObject.getAttachData())));
+                    } catch (WebStateException e) {
+                        // already logged
+                    }
+                })
+        );
+    }
 
     public static GroupResult sendBulk(final Set<Addressee> addressees, final String subject, final String body, List<Attachment> attachments) throws WebStateException {
         final CompletionService<MailResult> completionService = new ExecutorCompletionService<>(mailExecutor);
